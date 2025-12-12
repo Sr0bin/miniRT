d1d2bbd5c4353e84053b7c5f49379edfa20c3805
jweber <jweber@student.42Lyon.fr>
2025-12-12 11:21:18 +0100

    norm/rm: norm light_coef, and removed TODO to make specular object on each objects

--- src/render/light_coeff.c
+++ src/render/light_coeff.c
@@ -6,7 +6,7 @@
 /*   By: rorollin <rorollin@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 17:08:27 by rorollin          #+#    #+#             */
-/*   Updated: 2025/12/11 18:41:25 by rorollin         ###   ########.fr       */
+/*   Updated: 2025/12/12 11:20:12 by jweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,17 @@ double	coeff_direct_light(t_intersect	*ptr_intersect, t_object *ptr_light)
 	t_vec3	light_dir;
 
 	normal = intersect_normal(ptr_intersect);
-	light_dir = vect3_from_point3(ptr_intersect->intersect_point, ptr_light->coordinates);
-	temp = dot_product3(normal, light_dir); 
+	light_dir = vect3_from_point3(ptr_intersect->intersect_point,
+			ptr_light->coordinates);
+	temp = dot_product3(normal, light_dir);
 	temp = temp / (vec3_norm(normal) * vec3_norm(light_dir));
 	if (temp <= 0)
 		return (0);
 	return (temp * ptr_light->object_attr.light.brightness);
 }
 
-double	coeff_specular_light(t_intersect *ptr_intersect, t_ray ray, t_object *ptr_light)
+double	coeff_specular_light(t_intersect *ptr_intersect, t_ray ray,
+			t_object *ptr_light)
 {
 	double	coeff;
 	t_vec3	light_dir;
@@ -38,15 +40,15 @@ double	coeff_specular_light(t_intersect *ptr_intersect, t_ray ray, t_object *ptr
 	t_vec3	visibility;
 	t_vec3	normal;
 
-	light_dir = vect3_from_point3(ptr_intersect->intersect_point, ptr_light->coordinates);
-	normal = intersect_normal(ptr_intersect); 
+	light_dir = vect3_from_point3(ptr_intersect->intersect_point,
+			ptr_light->coordinates);
+	normal = intersect_normal(ptr_intersect);
 	reflection = reflection_vector3(light_dir, normal);
 	visibility = ray.direction;
 	coeff = dot_product3(reflection, visibility);
 	if (coeff <= 0)
 		return (0);
-	coeff = coeff /  ((vec3_norm(reflection) * vec3_norm(visibility)));
-	//TODO: make it on a per object basis
+	coeff = coeff / ((vec3_norm(reflection) * vec3_norm(visibility)));
 	coeff = pow(coeff, GLOBAL_SPECULAR);
 	return (coeff);
 }
