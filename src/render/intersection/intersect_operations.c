bfcb4bbba489518bf9eb23bab796a3e265a09750
jweber <jweber@student.42Lyon.fr>
2025-12-12 11:02:26 +0100

    norm: new function to reduce line number in file

--- src/render/intersection/intersect_operations.c
+++ src/render/intersection/intersect_operations.c
@@ -6,7 +6,7 @@
 /*   By: rorollin <rorollin@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 14:24:44 by rorollin          #+#    #+#             */
-/*   Updated: 2025/12/12 10:53:35 by jweber           ###   ########.fr       */
+/*   Updated: 2025/12/12 11:01:58 by jweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 #include "intersection.h"
 #include <stdio.h>
 
-static int		update_intersect_object(t_ray ray, t_object	*obj,
-					t_intersect *ptr_intersect);
-static void		update_closest_intersect(t_intersect *ptr_intersect,
-					t_intersect *ptr_intersect_tmp);
+static int	update_intersect_object(t_ray ray, t_object	*obj,
+				t_intersect *ptr_intersect);
+static void	update_closest_intersect(t_intersect *ptr_intersect,
+				t_intersect *ptr_intersect_tmp);
+static int	update_closest_intersect_return(t_intersect *ptr_intersect,
+				t_intersect *ptr_intersect_tmp);
 
 int	update_intersect_all_object(t_ray *ray, t_object *obj_array,
 		size_t count, t_intersect *ptr_intersect)
@@ -40,37 +42,34 @@ static int	update_intersect_object(t_ray ray, t_object	*obj,
 				t_intersect *ptr_intersect)
 {
 	t_intersect	intersect_data_tmp;
-	int			ret;
 
 	intersect_data_tmp.ptr_obj = obj;
 	if (obj->type == OBJ_SPHERE)
 	{
-		ret = check_intersect_sphere(ray, *obj, &intersect_data_tmp);
-		if (ret == TRUE)
-		{
-			update_closest_intersect(ptr_intersect, &intersect_data_tmp);
-			return (TRUE);
-		}
+		if (check_intersect_sphere(ray, *obj, &intersect_data_tmp) == TRUE)
+			return (update_closest_intersect_return(ptr_intersect,
+					&intersect_data_tmp));
 	}
 	else if (obj->type == OBJ_PLANE)
 	{
-		ret = check_intersect_plane(ray, *obj, &intersect_data_tmp);
-		if (ret == TRUE)
-		{
-			update_closest_intersect(ptr_intersect, &intersect_data_tmp);
-			return (TRUE);
-		}
+		if (check_intersect_plane(ray, *obj, &intersect_data_tmp) == TRUE)
+			return (update_closest_intersect_return(ptr_intersect,
+					&intersect_data_tmp));
 	}
 	else if (obj->type == OBJ_CYLINDER)
 	{
-		ret = check_intersect_cylinder(ray, *obj, &intersect_data_tmp);
-		if (ret == TRUE)
-		{
-			update_closest_intersect(ptr_intersect, &intersect_data_tmp);
-			return (TRUE);
-		}
+		if (check_intersect_cylinder(ray, *obj, &intersect_data_tmp) == TRUE)
+			return (update_closest_intersect_return(ptr_intersect,
+					&intersect_data_tmp));
 	}
 	return (FALSE);
+}
+
+int	update_closest_intersect_return(t_intersect *ptr_intersect,
+				t_intersect *ptr_intersect_tmp)
+{
+	update_closest_intersect(ptr_intersect, ptr_intersect_tmp);
+	return (TRUE);
 }
 
 static void	update_closest_intersect(t_intersect *ptr_intersect,
